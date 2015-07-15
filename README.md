382650dd92d7b87f46a5fcacf5cd75637dd3eedc
klghlch <kaleighleach@gmail.com>
2015-07-15 13:16:40 -0400

    pi1 page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ScrumBoard
+Easier project management
--- js/plugins/morris/morris-data.js
+++ js/plugins/morris/morris-data.js
@@ -6,80 +6,67 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '0',
+            estimated: 103,
+            actual: 103
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '1',
+            estimated: 93,
+            actual: 87
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2',
+            estimated: 83,
+            actual: 75
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '3',
+            estimated: 73,
+            actual: 68
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '4',
+            estimated: 63,
+            actual: 50
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '5',
+            estimated: 53,
+            actual: 40
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '6',
+            estimated: 43,
+            actual: 35
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '7',
+            estimated: 33,
+            actual: 24
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '8',
+            estimated: 23,
+            actual: 15
+            
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '9',
+            estimated: 13,
+            actual: 5
+           
+        },
+        {
+            period: '10',
+            estimated: 0,
+            actual: 1
+           
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
+        ykeys: ['estimated', 'actual'],
+        ymax: 105, 
+        labels: ['estimated', 'actual'],
+        pointSize: 3,
         hideHover: 'auto',
-        resize: true
+        resize: true, 
+        behaveLikeLine: true,
+        parseTime: false,
+
     });
 
-    // Donut Chart
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales",
-            value: 12
-        }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
-        }],
-        resize: true
-    });
-
+   
     // Line Chart
     Morris.Line({
         // ID of the element in which to draw the chart.
